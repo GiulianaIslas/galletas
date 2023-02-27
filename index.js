26e433b419502fa69687d70908f865ce9f80e703
José  Velázquez <josevelazquez2003@outlook.es>
2023-02-27 10:45:02 -0600

    Cambios varios, "start" ahora ejecuta nodemon en lugar de react-scripts

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 import express from 'express'
 
-
 const app = express()
-app.use(express.static('./dist'))
+app.use(express.static('./build'))
 
 const port = 3000;
 
-
 const listenFunction = () => console.log(`El servidor esta corriendo en el puerto : ${port}`)
 app.listen(port, listenFunction)
+
