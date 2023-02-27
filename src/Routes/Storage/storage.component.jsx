f1ecfbefd9f05edeaa44df71f8a9e92e503ebc49
Giuliana Sofía Islas <giuliislas@gmail.com>
2023-02-27 11:34:40 -0600

    Layout almacen

--- src/Routes/Storage/storage.component.jsx
+++ src/Routes/Storage/storage.component.jsx
@@ -1,4 +1,26 @@
-const Storage = () => {
+import {useState} from "react";
+import './storage.styles.scss';
+import IngredientForm from "../../Components/Ingredient-form/ingredient-form.component";
 
+const Storage = () => {
+    const defaultFormFields = {
+        quantity:'',
+        name:'',
+    }
+    const [formFields,setFormFields]=useState(defaultFormFields);
+    const {quantity,name } = formFields;
+
+    const handleChange = (event) => {
+        const {name,value} = event.target;
+        setFormFields({...formFields,[name]:value})
+    }
+    return(
+        <div className='background-img' style={{backgroundImage: `url('https://drive.google.com/uc?export=view&id=1Zt86tzbGgf8yrMlQRoGe5D5jtuORgR1J')`}}>
+            <div className='forms-container'>
+                <IngredientForm/>
+                <IngredientForm/>
+            </div>
+        </div>
+    );
 }
 export default Storage
