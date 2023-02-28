4fdd265db2526bb75dbb2dc3de85d0b0c90a0ad1
José  Velázquez <josevelazquez2003@outlook.es>
2023-02-28 12:04:30 -0600

    Agregado mongodb

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from 'express'
+import mongoose from 'mongoose';
 
+// Conectar con mongodb
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/restapis',{
+    useNewUrlParser: true
+})
+
+// Crear la app del servidor
 const app = express()
 app.use(express.static('./build'))
 
+// Definir el puerto y arrancar el servidor
 const port = 3000;
 
 const listenFunction = () => console.log(`El servidor esta corriendo en el puerto : ${port}`)
