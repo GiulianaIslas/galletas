32fac9103b292bc1bbc236e5ced75f935381b6c6
José  Velázquez <josevelazquez2003@outlook.es>
2023-03-09 23:17:33 -0600

    last gets added

--- routes/router.js
+++ routes/router.js
@@ -1,13 +1,17 @@
 import express from "express"
 
-import { addIngredient } from '../controllers/warehouseController.js'
-import { editRecipe, validateRecipe, deleteRecipe } from '../controllers/recipeController.js'
+import { addIngredient, getAllIngredients } from '../controllers/warehouseController.js'
+import { editRecipe, validateRecipe, deleteRecipe, getAllRecipes } from '../controllers/recipeController.js'
 
 const router = express.Router()
 
 router.post('/addIngredient', addIngredient)
 
+router.get('/getAllIngredients', getAllIngredients)
+
 router.get('/validateRecipe/:id', validateRecipe)
+
+router.get('/getAllRecipes', getAllRecipes)
 
 router.put('/editRecipe', editRecipe)
 
