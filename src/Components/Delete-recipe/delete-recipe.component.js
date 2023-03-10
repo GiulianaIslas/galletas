3046f9eb37b3125d21ea5abe282b6d1fb904da46
Giuliana Sofía Islas <giuliislas@gmail.com>
2023-03-10 00:23:40 -0600

    Connected Delete Recipe endpoints

--- src/Components/Delete-recipe/delete-recipe.component.js
+++ src/Components/Delete-recipe/delete-recipe.component.js
@@ -5,26 +5,19 @@ import './delete-recipe.styles.scss';
 
 const DeleteRecipe = () => {
     //////ENDPOINT GET recipeList
-    const recipes = [
-        {
-            id:574221324,
-            name:'chispas',
-        },
-        {
-            id:458621754,
-            name:'mantequilla',
-        },
-        {
-            id:789357841,
-            name:'chocolate',
-        }
-    ];
+    const fetchRecipes = async () =>{
+        await fetch ('http://localhost:3000/api/getAllRecipes')
+            .then((data)=>{return data.json()});
+    };
+    const options = fetchRecipes();
+
     const defaultRecipe = {
         id:'',
     };
     const [recipe,setRecipe] = useState(defaultRecipe);
     const {id} = recipe;
     const [index,setIndex] = useState(0);
+    const [success,setSuccess] = useState(false);
     const handleSelectChange = ({value}) => {
         setRecipe({...recipe,['id']:value});
     }
@@ -35,12 +28,11 @@ const DeleteRecipe = () => {
     }
     const submitData = (event) => {
         event.preventDefault();
-        fetch('http://localhost:3000/api/deleteRecipe/{id}',{
-            method: "POST",
-            body: JSON.stringify(recipe),
+        fetch(`http://localhost:3000/api/deleteRecipe/${id}`,{
+            method: "DELETE",
         }).then(response=>response.json())
-            .then(json=> console.log(json));
-        console.log(JSON.stringify(recipe));
+            .then(json=> console.log(json))
+            .then(()=> setSuccess(true));
         setIndex(1);
     }
 
@@ -50,17 +42,25 @@ const DeleteRecipe = () => {
                 <h2 >Eliminar Receta</h2>
                 <form onSubmit={submitData} className='delete-form-container'>
                     <div className='select'>
-                        <Select onChange={handleSelectChange} options={recipes.map(sup => ({label:sup.name,value:sup.id}))}/>
+                        <Select onChange={handleSelectChange} options={options.map(sup => ({label:sup.name,value:sup.id}))}/>
                     </div>
                     <Button buttonType='dark' type='submit'>ACEPTAR</Button>
                 </form>
             </div>
         );
-    if(index===1)
+    if(index===1 && success)
         return(
             <div className='delete-container'>
                 <h2 >Eliminar Receta</h2>
                 <p>La receta fue eliminada exitosamente del inventario. </p>
+                <Button type='button'  buttonType='dark' onClick={handleClick}>REGRESAR</Button>
+            </div>
+        );
+    if (index===1 && !success)
+        return(
+            <div className='delete-container'>
+                <h2 >Error!</h2>
+                <p>La receta no fue eliminada exitosamente del inventario. </p>
                 <Button type='button'  buttonType='dark' onClick={handleClick}>REGRESAR</Button>
             </div>
         );
